22a775fdcebb667eb65ec3466e2eec9b313a6ea7
Alex Ov <alexov@swisscows.email>
2024-11-02 20:48:05 +0200

    Add code for remote browser run.

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
 
 import static constants.Constants.TimeoutVariable.IMPLICIT_WAIT;
 import static constants.Constants.Url.REGISTRATION_URL;
@@ -22,10 +27,38 @@ public class TestBase {
 
     @BeforeEach
     public void setUp() {
-        driver = new ChromeDriver();
+
+        boolean isRemote = Boolean.parseBoolean(System.getProperty("isRemote", "false"));
+
+        if (isRemote) {
+
+            ChromeOptions options = new ChromeOptions();
+
+            options.setCapability("browserVersion", "100.0");
+
+            options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+
+                /* How to enable video recording */
+                put("enableVideo", true);
+                put("enableVNC", true);
+
+            }});
+
+            try {
+                this.driver = new RemoteWebDriver(new URL("https://user1:1234@selenoid.autotests.cloud/wd/hub"), options);
+            } catch (MalformedURLException e) {
+                throw new IllegalArgumentException("Bad Selenoid URL");
+            }
+
+
+        } else {
+            driver = new ChromeDriver();
+        }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
         driver.get(REGISTRATION_URL);
+
     }
 
     @AfterEach
