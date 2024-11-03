e13559a9fb0c062228654488f5e7f11e4040d415
Assem Saginayeva <asemsaginayeva@gmail.com>
2024-11-03 23:18:33 +0100

    added Attach, updated TestBase, build.gradle to screenshots and attachments

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -3,55 +3,56 @@ package helpers;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
-import static org.openqa.selenium.logging.LogType.BROWSER;
+public class Attach {
+    @Attachment(value = "{attachName}", type = "image/png")
+    public static byte[] screenshotAs(String attachName, WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
 
-//public class Attach {
-//    @Attachment(value = "{attachName}", type = "image/png")
-//    public static byte[] screenshotAs(String attachName) {
-//        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
-//    }
-//
-//    @Attachment(value = "Page source", type = "text/plain")
-//    public static byte[] pageSource() {
-//        return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
-//    }
-//
-//    @Attachment(value = "{attachName}", type = "text/plain")
-//    public static String attachAsText(String attachName, String message) {
-//        return message;
-//    }
-//
-//    public static void browserConsoleLogs() {
-//        attachAsText(
-//                "Browser console logs",
-//                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
-//        );
-//    }
-//
-//    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-//    public static String addVideo() {
-//        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-//                + getVideoUrl()
-//                + "' type='video/mp4'></video></body></html>";
-//    }
-//
-//    public static URL getVideoUrl() {
-//        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
-//        try {
-//            return new URL(videoUrl);
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//    public static String getSessionId(){
-//        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
-//    }
-//}
+    @Attachment(value = "Page source", type = "text/plain")
+    public static byte[] pageSource(WebDriver driver) {
+        return driver.getPageSource().getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Attachment(value = "{attachName}", type = "text/plain")
+    public static String attachAsText(String attachName, String message) {
+        return message;
+    }
+
+    public static void browserConsoleLogs(WebDriver driver) {
+        LogEntries logs = driver.manage().logs().get(LogType.BROWSER);
+        String logMessages = logs.getAll().stream()
+                .map(LogEntry::getMessage)
+                .collect(Collectors.joining("\n"));
+        attachAsText("Browser console logs", logMessages);
+    }
+
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String addVideo(WebDriver driver) {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + getVideoUrl(driver)
+                + "' type='video/mp4'></video></body></html>";
+    }
+
+    public static URL getVideoUrl(WebDriver driver) {
+        String sessionId = ((RemoteWebDriver) driver).getSessionId().toString();
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
+        try {
+            return new URL(videoUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,24 @@
 package tests;
 
+import helpers.Attach;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import pages.RegistrationPage;
+import pages.SearchPage;
+import utils.TestData;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
 
 import static constants.Constants.TimeoutVariable.IMPLICIT_WAIT;
 import static constants.Constants.Url.REGISTRATION_URL;
@@ -22,16 +33,68 @@ public class TestBase {
 
     @BeforeEach
     public void setUp() {
-        driver = new ChromeDriver();
+        boolean isRemote = Boolean.parseBoolean(System.getProperty("isRemote", "false"));
+
+        if (isRemote) {
+            ChromeOptions options = new ChromeOptions();
+            options.setCapability("browserVersion", "100.0");
+            options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+                /* How to enable video recording */
+                put("enableVideo", true);
+                put("enableVNC", true);
+                put("name", "Test badge ");
+            }});
+
+            try {
+                this.driver = new RemoteWebDriver(new URL("https://user1:1234@selenoid.autotests.cloud/wd/hub"), options);
+            } catch (MalformedURLException e) {
+                throw new IllegalArgumentException("Bad Selenoid URL");
+            }
+
+        } else {
+            driver = new ChromeDriver();
+        }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
         driver.get(REGISTRATION_URL);
     }
 
     @AfterEach
-    void tearDown() {
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot", driver);
+        Attach.pageSource(driver);
+        Attach.browserConsoleLogs(driver);
+        Attach.addVideo(driver);
+
         if (driver != null) {
             driver.quit();
         }
+    }
+
+    public void registerAndAddProductToCart() {
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        TestData testData = new TestData();
+        String firstName = testData.getFirstName();
+        String lastName = testData.getLastName();
+        String email = testData.getUserEmail();
+        String password = testData.getPassword();
+
+        registrationPage.clickRegisterLink();
+        registrationPage.fillRegistrationForm(firstName, lastName, email, password);
+        registrationPage.submitRegistration();
+
+        SearchPage searchPage = new SearchPage(driver);
+        searchPage.searchForItem("computer");
+        searchPage.findProductByName("Simple Computer");
+        searchPage.findProductByPrice(BigDecimal.valueOf(800.00).setScale(2, RoundingMode.HALF_UP));
+        searchPage.clickOnProductLink("Simple Computer");
+        searchPage.clickRadioButton("Slow");
+        searchPage.clickRadioButton("4 GB");
+        searchPage.clickRadioButton("400 GB");
+        searchPage.clickCheckbox("Office Suite");
+        searchPage.verifyProductName("Simple Computer");
+        searchPage.verifyProductPrice(BigDecimal.valueOf(800.00));
+        searchPage.clickAddToCartButton();
     }
 }
