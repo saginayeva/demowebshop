ef0700d130c23cf95fb7fc30e68aa04fbea9605a
asaginayeva <assemgul.saginayeva@generalbytes.com>
2024-11-05 17:24:33 +0100

    updated TestBase to running for CI

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,6 +22,9 @@ import static constants.Constants.Url.REGISTRATION_URL;
 public class TestBase {
     protected WebDriver driver;
     protected static boolean isRemote;
+    public static boolean isRunningInCICD() {
+        return "true".equals(System.getenv("GITHUB_ACTIONS"));
+    }
 
     @BeforeAll
     static void beforeAll() {
@@ -31,36 +34,42 @@ public class TestBase {
 
     @BeforeEach
     public void setUp() {
-        if (isRemote) {
-            ChromeOptions options = new ChromeOptions();
-            options.setCapability("browserVersion", "100.0");
-            options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-                /* How to enable video recording */
-                put("enableVideo", true);
-                put("enableVNC", true);
-                put("name", "Test badge ");
-            }});
-
-            try {
-                this.driver = new RemoteWebDriver(new URL("https://user1:1234@selenoid.autotests.cloud/wd/hub"), options);
-            } catch (MalformedURLException e) {
-                throw new IllegalArgumentException("Bad Selenoid URL");
-            }
-
-        } else {
-            driver = new ChromeDriver();
-        }
-
+        this.driver = initializeDriver();
         DriverContainer.setDriver(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
         driver.get(REGISTRATION_URL);
     }
 
+    private WebDriver initializeDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox", "--disable-dev-shm-usage", "--disable-popup-blocking", "--remote-allow-origins=*", "--window-size=1920,1080");
+
+        if (isRunningInCICD()) {
+            options.addArguments("--headless", "--disable-gpu", "--disable-extensions");
+        }
+
+        if (isRemote) {
+            options.setCapability("browserVersion", "100.0");
+            options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+                put("enableVideo", true);
+                put("enableVNC", true);
+                put("name", "Test badge ");
+            }});
+
+            try {
+                return new RemoteWebDriver(new URL("https://user1:1234@selenoid.autotests.cloud/wd/hub"), options);
+            } catch (MalformedURLException e) {
+                throw new IllegalArgumentException("Bad Selenoid URL", e);
+            }
+        } else {
+            return new ChromeDriver(options);
+        }
+    }
+
     @AfterEach
     void addAttachments() {
         attachAttachments();
-
         if (driver != null) {
             driver.quit();
         }
